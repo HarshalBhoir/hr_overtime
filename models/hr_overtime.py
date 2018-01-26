0506053c51cb8f6589376d9c2b867d0eba7ea398
aymanshow <aymanmcarosoft@gmail.com>
2018-01-26 02:30:55 +0200

    Update hr_overtime.py
    This class is not nedeed anymore since V9. Just it only for compatibility

--- models/hr_overtime.py
+++ models/hr_overtime.py
@@ -29,6 +29,12 @@ from openerp import models, fields, api, _
 from openerp.exceptions import except_orm, Warning, RedirectWarning
 from openerp.tools import float_compare
 import openerp.addons.decimal_precision as dp
+import logging
+
+_logger = logging.getLogger(__name__)
+
+class ir_sequance_type(models.Model):
+	_name = 'ir.sequence.type'
 
 class hr_overtime(models.Model):
 	_name = "hr.overtime"
